3c6189dcd3251f6561df964f0d438a719a059dc1
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2020-06-18 16:58:37 +0300

    Updates

---
+++ precache-manifest.d8cb81531c6ea5971ccab85ae24bd90e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1241b6ccdb588e8d695b0ab9249a4764",
+    "url": "/YesOrNo-App/index.html"
+  },
+  {
+    "revision": "8c24129ee16da08dc8f7",
+    "url": "/YesOrNo-App/static/js/2.772ab68d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/YesOrNo-App/static/js/2.772ab68d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d68ea5e6da09824163",
+    "url": "/YesOrNo-App/static/js/main.ec376cff.chunk.js"
+  },
+  {
+    "revision": "065b717b3061720850f6",
+    "url": "/YesOrNo-App/static/js/runtime-main.0f8eeac0.js"
+  }
+]);
